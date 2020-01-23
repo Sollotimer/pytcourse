2060d719226244801b2d5e10544bac6790c5a971
Sollotimer <39565715+Sollotimer@users.noreply.github.com>
2020-01-24 00:18:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+import kivy
+kivy.require('1.10.1')
+
+class MyApp(App):
+    def build (self):
+
+        fl = FloatLayout(size = (300, 300))
+        fl.add_widget(Button(
+        text = "this is my button",
+        background_color = [1, 0, 0, 1],
+        size_hint = (.5, .25)));
+
+    return fl
+
+    def btn_press(self, instance):
+        print('button pressed')
+        instance.text = 'ready'
+
+MyApp().run()
